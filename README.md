6f4be0112c52bdd4612a1f8b57a6a2cdba61c396
saurav1404 <sanspoly@gmail.com>
2019-05-27 00:43:39 +0530

    0.0.3-pre

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # astra-ui
 react ui component library
 
+## Install
+
+```bash
+npm install astra-ui
+```
+
+## Usage
+
+```jsx
+import { Button } from 'astra-ui';
+ReactDOM.render(<Button />, mountNode);
+```
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import React from "react";
-
-const ReactColorSquare = props => {
-  const { width, height, color, text } = props;
-  return (
-    <div
-      style={{
-        width: width || 100,
-        height: height || 100,
-        backgroundColor: color || "blue"
-      }}
-    >
-      {text}
-    </div>
-  );
-};
-
-export default ReactColorSquare;
+export * from './components';
