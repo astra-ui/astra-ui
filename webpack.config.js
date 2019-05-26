04dbf41624d03278127050393c4a03ad1ae2b848
saurav1404 <sanspoly@gmail.com>
2019-05-27 01:30:26 +0530

    latest package with build fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 module.exports = {
-  mode: "development",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
+    publicPath: '',
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
@@ -19,9 +19,36 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      {
+				// I haven't used SCSS in the base example, but it's here for you if you
+				// want! If you want to use CSS, you can change this next like's regex to
+				// /\.(css|scss)$/ or even just /\.css$/
+				test: /\.scss$/,
+				use: [
+					// These three libraries are commonly used together to turn Sass into
+					// CSS, then be able to load the CSS directly with imports. From there
+					// It gets put in the DOM for you.
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' },
+				],
+			},
+			{
+				// Some image formats so you can import images
+				test: /\.(png|gif|jpg|svg)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 50000,
+					},
+				},
+			},
     ]
   },
+  resolve: {
+		extensions: ['.scss', '.js', '.json', '.png', '.gif', '.jpg', '.svg'],
+	},
   externals: {
     'react': 'commonjs react' 
   }
